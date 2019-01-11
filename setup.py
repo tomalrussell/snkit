7c64d8750f8110ee7966505e2b583f65c9a483ab
Tom Russell <tomalrussell@gmail.com>
2019-01-11 17:13:39 +0000

    Retry PyPI auth

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='snkit',
-    version='0.1.0',
+    version='0.1.2',
     license='MIT License',
     description='a spatial networks toolkit',
     long_description='',
