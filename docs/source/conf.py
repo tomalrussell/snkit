629d8ba0ff2623ed417e03448914f477b89a3dd7
Tom Russell <tomalrussell@gmail.com>
2019-08-28 11:24:50 +0100

    Configure m2r extension for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,6 +90,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'm2r',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -97,9 +98,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
